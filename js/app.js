34e5174ed795bfc887641944dfe7701fe370b1f7
Luciano Angeleri <lcnang@gmail.com>
2023-04-18 07:18:14 -0300

    Se agregó la funcionalidad principal del temporizador

--- js/app.js
+++ js/app.js
@@ -7,48 +7,86 @@ let horas = document.getElementById("horas"),
     segundos = document.getElementById("segundos"),
     milisegundos = document.getElementById("milisegundos");
 
+let inputHoras = document.getElementById("inputHoras"),
+    inputMinutos = document.getElementById("inputMinutos"),
+    inputSegundos = document.getElementById("inputSegundos"),
+    inputMilisegundos = document.getElementById("inputMilisegundos");
+
 btnInicio.addEventListener("click", iniciar);
 btnPausa.addEventListener("click", pausar);
 btnReset.addEventListener("click", reset);
 
+
+let tiempoFinal;
 let tiempoActual;
-let tiempoCronometrado = 0;
-let intervalo;
+let tiempoFaltante;
+let actualizar;
 let mseg = 0;
 let seg = 0;
 let min = 0;
 let hs = 0;
 
-function cronometrarTiempo() {
-    tiempoCronometrado = Date.now() - tiempoActual;
-    mseg = tiempoCronometrado % 1000;
-    seg = Math.floor(tiempoCronometrado/1000)%60;
-    min = Math.floor(tiempoCronometrado/1000/60)%60;
-    hs = Math.floor(tiempoCronometrado/1000/60/60);
+function setearTiempo(){
+    mseg = parseInt(inputMilisegundos.value ? inputMilisegundos.value : 0);
+    seg = parseInt(inputSegundos.value ? inputSegundos.value : 0);
+    min = parseInt(inputMinutos.value ? inputMinutos.value : 0);
+    hs = parseInt(inputHoras.value ? inputHoras.value : 0);
+    tiempoFinal = new Date().getTime() + (mseg + (seg * 1000) + (min * 60 * 1000)+ (hs * 60 *60 * 1000));
 }
+function reiniciarBotones() {
+        btnReset.disabled = true
+        btnPausa.disabled = true
+        btnInicio.disabled = false
+        btnPausa.classList.replace("btn-outline-primary","btn-outline-secondary")
+        btnReset.classList.replace("btn-outline-danger","btn-outline-secondary")
+        if (btnInicio.classList.contains("btn-outline-secondary")) {
+            btnInicio.classList.replace("btn-outline-secondary","btn-outline-success")
+        }
+}
+
 function actualizarTiempo() {
-    if (mseg < 10) {
-        mseg = "00" + mseg
-    }else if(mseg != 10 && mseg < 100){
-        mseg = "0" + mseg
+    tiempoActual = new Date().getTime();
+    tiempoFaltante = tiempoFinal - tiempoActual;
+    if (tiempoFaltante <= 0) {
+        alert("Tiempo Finalizado!")
+        clearInterval(actualizar)
+        tiempoFinal  = 0;
+        tiempoActual  = 0;
+        tiempoFaltante = 0;
+        mseg = 0;
+        seg = 0;
+        min = 0;
+        hs = 0;
+        reiniciarBotones();
+        
     }
-    if (seg < 10) {
-        seg = "0" + seg
-    }
-    if (min < 10) {
-        min = "0" + min
-    }
-    if (hs < 10) {
-        hs = "0" + hs
-    }
-    milisegundos.innerHTML = `${mseg}<small class="fs-6 ps-1 d-none d-md-inline">mseg</small>`
-    segundos.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`
-    minutos.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`
-    horas.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`
+        mseg = tiempoFaltante % 1000;
+        seg = Math.floor(tiempoFaltante/1000)%60;
+        min = Math.floor(tiempoFaltante/1000/60)%60;
+        hs = Math.floor(tiempoFaltante/1000/60/60);
+        if (mseg < 10) {
+            mseg = "00" + mseg
+        }else if(mseg != 10 && mseg < 100){
+            mseg = "0" + mseg
+        }
+        if (seg < 10) {
+            seg = "0" + seg
+        }
+        if (min < 10) {
+            min = "0" + min
+        }
+        if (hs < 10) {
+            hs = "0" + hs
+        }
+        milisegundos.innerHTML = `${mseg}<small class="fs-6 ps-1 d-none d-md-inline">mseg</small>`
+        segundos.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`
+        minutos.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`
+        horas.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`
+    
+
 }
 function iniciar() {
-    tiempoActual = Date.now() - tiempoCronometrado;
-    intervalo = setInterval(cronometrarTiempo, 1)
+    setearTiempo()
     actualizar = setInterval(actualizarTiempo, 1)
     btnInicio.disabled = true
     btnPausa.disabled = false
@@ -59,7 +97,6 @@ function iniciar() {
 
 }
 function pausar() {
-    clearInterval(intervalo);
     clearInterval(actualizar);
     btnInicio.disabled = false
     btnPausa.disabled = true
@@ -68,20 +105,17 @@ function pausar() {
 
 }
 function reset() {
-    clearInterval(intervalo);
     clearInterval(actualizar);
-    tiempoCronometrado = 0;
+    tiempoFinal  = 0;
+    tiempoActual  = 0;
+    tiempoFaltante = 0;
     mseg = 0;
     seg = 0;
     min = 0;
     hs = 0;
-    actualizarTiempo();
-    btnReset.disabled = true
-    btnPausa.disabled = true
-    btnInicio.disabled = false
-    btnPausa.classList.replace("btn-outline-primary","btn-outline-secondary")
-    btnReset.classList.replace("btn-outline-danger","btn-outline-secondary")
-    if (btnInicio.classList.contains("btn-outline-secondary")) {
-        btnInicio.classList.replace("btn-outline-secondary","btn-outline-success")
-    }
+    milisegundos.innerHTML = `${mseg}<small class="fs-6 ps-1 d-none d-md-inline">mseg</small>`
+    segundos.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`
+    minutos.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`
+    horas.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`
+    reiniciarBotones()
 }
