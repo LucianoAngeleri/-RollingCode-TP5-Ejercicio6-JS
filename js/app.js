b1df6b2fc16343cdad059f1566d8b43963c552e7
Luciano Angeleri <lcnang@gmail.com>
2023-04-18 09:05:16 -0300

    Se agregaron las demas funcionalidades para pausar y resetear, asi como tambien la validacion para los campos vacíos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let btnInicio = document.getElementById("btnInicio"),
     btnPausa = document.getElementById("btnPausa"),
-    btnReset = document.getElementById("btnReset");
+    btnReset = document.getElementById("btnReset"),
+    modalFinalizar = document.getElementById("modalFinalizar");
+    modalAdvertencia = document.getElementById("modalAdvertencia");
 
 let horas = document.getElementById("horas"),
     minutos = document.getElementById("minutos"),
@@ -20,6 +22,7 @@ btnReset.addEventListener("click", reset);
 let tiempoFinal;
 let tiempoActual;
 let tiempoFaltante;
+let tiempoFaltanteGuardado = 0;
 let actualizar;
 let mseg = 0;
 let seg = 0;
@@ -43,50 +46,74 @@ function reiniciarBotones() {
             btnInicio.classList.replace("btn-outline-secondary","btn-outline-success")
         }
 }
+function escribirTiempo() {
+    milisegundos.innerHTML = `${mseg}<small class="fs-6 ps-1 d-none d-md-inline">mseg</small>`
+    segundos.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`
+    minutos.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`
+    horas.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`
+}
+function resetearInputs() {
+    inputHoras.value = "";
+    inputMinutos.value = "";
+    inputSegundos.value = "";
+    inputMilisegundos.value = "";
+}
+function mostrarModalFinalizar(){
+    var modal = new bootstrap.Modal(modalFinalizar);
+    modal.show();
+}
+function mostrarModalAdvertencia(){
+    var modal = new bootstrap.Modal(modalAdvertencia);
+    modal.show();
+}
 
 function actualizarTiempo() {
-    tiempoActual = new Date().getTime();
-    tiempoFaltante = tiempoFinal - tiempoActual;
-    if (tiempoFaltante <= 0) {
-        alert("Tiempo Finalizado!")
-        clearInterval(actualizar)
-        tiempoFinal  = 0;
-        tiempoActual  = 0;
-        tiempoFaltante = 0;
-        mseg = 0;
-        seg = 0;
-        min = 0;
-        hs = 0;
-        reiniciarBotones();
-        
+        tiempoActual = new Date().getTime();
+        tiempoFaltante = tiempoFinal - tiempoActual;
+        if (tiempoFaltante <= 0) {
+            mostrarModalFinalizar();
+            tiempoFinal  = 0;
+            tiempoActual  = 0;
+            tiempoFaltante = 0;
+            tiempoFaltanteGuardado = 0;
+            mseg = 0;
+            seg = 0;
+            min = 0;
+            hs = 0;
+            resetearInputs();
+            reiniciarBotones();
+            clearInterval(actualizar)
+        }
+            mseg = tiempoFaltante % 1000;
+            seg = Math.floor(tiempoFaltante/1000)%60;
+            min = Math.floor(tiempoFaltante/1000/60)%60;
+            hs = Math.floor(tiempoFaltante/1000/60/60);
+            if (mseg < 10) {
+                mseg = "00" + mseg
+            }else if(mseg != 10 && mseg < 100){
+                mseg = "0" + mseg
+            }
+            if (seg < 10) {
+                seg = "0" + seg
+            }
+            if (min < 10) {
+                min = "0" + min
+            }
+            if (hs < 10) {
+                hs = "0" + hs
+            }
+            escribirTiempo();
     }
-        mseg = tiempoFaltante % 1000;
-        seg = Math.floor(tiempoFaltante/1000)%60;
-        min = Math.floor(tiempoFaltante/1000/60)%60;
-        hs = Math.floor(tiempoFaltante/1000/60/60);
-        if (mseg < 10) {
-            mseg = "00" + mseg
-        }else if(mseg != 10 && mseg < 100){
-            mseg = "0" + mseg
-        }
-        if (seg < 10) {
-            seg = "0" + seg
-        }
-        if (min < 10) {
-            min = "0" + min
-        }
-        if (hs < 10) {
-            hs = "0" + hs
-        }
-        milisegundos.innerHTML = `${mseg}<small class="fs-6 ps-1 d-none d-md-inline">mseg</small>`
-        segundos.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`
-        minutos.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`
-        horas.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`
-    
-
-}
 function iniciar() {
-    setearTiempo()
+    if(tiempoFaltanteGuardado === 0){
+        setearTiempo()
+    } else {
+        tiempoFinal = new Date().getTime() + tiempoFaltanteGuardado
+    }
+    if (mseg===0 && seg===0 && min ===0 && hs===0) {
+        mostrarModalAdvertencia();
+        return;
+    }
     actualizar = setInterval(actualizarTiempo, 1)
     btnInicio.disabled = true
     btnPausa.disabled = false
@@ -94,9 +121,9 @@ function iniciar() {
     btnPausa.classList.replace("btn-outline-secondary","btn-outline-primary")
     btnReset.classList.replace("btn-outline-secondary","btn-outline-danger")
     btnInicio.classList.replace("btn-outline-success","btn-outline-secondary")
-
 }
 function pausar() {
+    tiempoFaltanteGuardado = tiempoFaltante
     clearInterval(actualizar);
     btnInicio.disabled = false
     btnPausa.disabled = true
@@ -109,13 +136,12 @@ function reset() {
     tiempoFinal  = 0;
     tiempoActual  = 0;
     tiempoFaltante = 0;
+    tiempoFaltanteGuardado=0
     mseg = 0;
     seg = 0;
     min = 0;
     hs = 0;
-    milisegundos.innerHTML = `${mseg}<small class="fs-6 ps-1 d-none d-md-inline">mseg</small>`
-    segundos.innerHTML = `${seg}<small class="fs-6 ps-1 d-none d-md-inline">seg</small>`
-    minutos.innerHTML = `${min}<small class="fs-6 ps-1 d-none d-md-inline">min</small>`
-    horas.innerHTML = `${hs}<small class="fs-6 ps-1 d-none d-md-inline">hs</small>`
+    escribirTiempo();
     reiniciarBotones()
+    resetearInputs();
 }
